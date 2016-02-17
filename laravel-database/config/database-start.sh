770e4c4aee47d0493026e36839ab3097b5fb3472
Aleksey Sidoryak <a@sidoryak.ru>
2016-02-17 14:14:12 +0000

    Update database-start.sh

--- laravel-database/config/database-start.sh
+++ laravel-database/config/database-start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # if the /data/storage/database directory doesn't contain anything, then initialise it
-MYSQL_DIR="/data/storage/database/data"
+MYSQL_DIR="/data/storage/database"
+MYSQL_DATA_DIR="${MYSQL_DIR}/data"
+
+if [ ! -d "${MYSQL_DIR}" ]; then
+    mkdir -p "${MYSQL_DIR}"
+fi
+
 if [ ! "$(ls -A ${MYSQL_DIR})" ]; then
-    /usr/bin/mysql_install_db --datadir=${MYSQL_DIR} --user=mysql
+    /usr/bin/mysql_install_db --datadir=${MYSQL_DATA_DIR} --user=mysql
     /usr/bin/mysqld_safe --init-file=/opt/bin/database-setup.sql
 else
     /usr/bin/mysqld_safe
